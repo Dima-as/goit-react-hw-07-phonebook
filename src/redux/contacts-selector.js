31e5c509fb2cae309d14ea51bfdda9af2d222b5b
Dima-as <d.sichenka@gmail.com>
2022-02-06 02:10:22 +0200

    fix

--- src/redux/contacts-selector.js
+++ src/redux/contacts-selector.js
@@ -1,10 +1,14 @@
+import { createSelector } from "@reduxjs/toolkit";
 export const getFilter = (state) => state.filter;
 export const getContacts = (state) => state.contacts.items;
 
-export const getVisibleContacts = (state) => {
-  const contacts = getContacts(state);
-  const filter = getFilter(state);
-  const normFilter = filter.toLowerCase();
+export const getVisibleContacts = createSelector(
+  [getContacts, getFilter],
+  (contacts, filter) => {
+    const normFilter = filter.toLowerCase();
 
-  return contacts.filter(({ name }) => name.toLowerCase().includes(normFilter));
-};
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(normFilter)
+    );
+  }
+);
